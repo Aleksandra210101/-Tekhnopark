9a6dfb205075eeeae6dafe75845402fb296ee5a6
Aleksandra210101 <krylovaping@yandex.ru>
2019-03-12 22:37:27 +0300

    Добавлено рассмотрение всех случаев аргументов

--- hw1/equation.py
+++ hw1/equation.py
@@ -1,6 +1,6 @@
 """
 Разработала: Крылова Александра
-Проект: решение квадратичного уравнения
+Проект: решение квадратичного уравнения axx+bx+c
 """
 
 import math
@@ -9,15 +9,24 @@ A = float(input("a = "))
 B = float(input("b = "))
 C = float(input("c = "))
 DISCR = B ** 2 - 4 * A * C
-if DISCR > 0:
-    X1 = (-B + math.sqrt(DISCR)) / (2 * A)
-    X2 = (-B - math.sqrt(DISCR)) / (2 * A)
-    print('x1 = %.2f, x2 = %.2f' % (X1, X2))
-elif DISCR == 0:
-    if A == 0:
-        X = 0
+print(DISCR)
+if DISCR > 0.0:
+    if A != 0.0:
+        X1 = (-B + math.sqrt(DISCR)) / (2 * A)
+        X2 = (-B - math.sqrt(DISCR)) / (2 * A)
+        print('x1 = %.2f, x2 = %.2f' % (X1, X2))
+    elif A == 0 and B != 0 and C != 0:
+        X = -C / B
+        print(X)
+    elif B != 0 and A == 0 and C == 0:
+        print("x = ", 0)
+    else:
+        print("Нет корней")
+elif DISCR == 0.0:
+    if A == 0.0 and B == 0.0:
+        print("Нет корней")
     else:
         X = -B / (2 * A)
-    print("x = %.2f" % X)
+        print("x = %.2f" % X)
 else:
     print("Корней нет")
